b8f50bcc6aa7f8c4ffc9711160f67d1223f77197
Remy Sharp <remy@leftlogic.com>
2010-03-22 10:08:46 +0000

    Simplified JavaScript and adding print styles

--- index.php
+++ index.php
@@ -18,11 +18,52 @@ if (isset($_POST['email']) && $_POST['email'] && validEmail($_POST['email'])) {
 }
 ?>
 <!DOCTYPE html>
+<!--
+
+                 ----~~~====*******====~~~----
+
+Hello there! \o,
+
+So you're interested in my sauce then? Good for you - many a developer has learnt from 
+popping the hood on web pages and sniffing around the source code. Since you've come 
+this far already, I thought I'd outline the juicy bits I've left for you in my code:
+
+1. We're using a "version-less" doctype, which switches to HTML5
+2. <script> tags go without the type attribute
+3. I'm using the latest version of the HTML5 shiv (http://bit.ly/cm2Lrb)
+4. I've used HTML5 elements where possible, and in particular, am using the <details>
+   element with a sprinkle of JavaScript - which I'll come on to
+5. I'm also using the placeholder attribute
+
+So, new sexy HTML5 elements, eh? Yep, no problem. I'm using <details> which is supposed
+to be interactive (but not currently native), and I'm using the placeholder attribute 
+which is only natively supported in Safari and Chrome. So to make this work across 
+browsers, I've added a  sprinkle of JavaScript - it's all sitting in 
+http://2010.full-frontal.org/html5.js
+
+In there you'll find patching JavaScript to add support to placeholder and autofocus
+and further in the script you'll find the code to support <details>. It's all fairly
+new so if you find a bug, let me know via Twitter @rem.
+
+Otherwise, thanks for stopping by, and hopefully you can make it to my conference,
+I planning on making it just as awesome as last year!
+
+Cheers,
+
+- Remy.
+
+                  ----~~~====*******====~~~----
+
+-->
 <html lang="en">
 <head>
 <meta charset=utf-8 />
 <title>Full Frontal - JavaScript Conference - 12th November 2010</title>
 <link rel="stylesheet" href="holding2010.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="print.css" type="text/css" media="print" />
+<!--[if lte IE 7]>
+<link rel="stylesheet" href="ie.css" type="text/css" media="screen" charset="utf-8" />
+<![endif]-->
 <script>
 // enable elements for styling in IE
 /*@cc_on'abbr article aside audio canvas details figcaption figure footer header hgroup mark menu meter nav output progress section summary time video'.replace(/\w+/g,function(n){document.createElement(n)})@*/
@@ -67,9 +108,10 @@ if (isset($_POST['email']) && $_POST['email'] && validEmail($_POST['email'])) {
     <div class="clear"></div>
     <section class="tweets">
       <h1>What people are saying</h1>
+      <!-- from: http://twitter.com/fullfrontalconf/favorites -->
       <ul id="tweets">
         <?php
-        $favs = json_decode(preg_replace('/^callback\((.*?)\)$/', '$1', file_get_contents('./fullfrontalconf.json')));
+        $favs = json_decode(file_get_contents('./fullfrontalconf.json'));
         shuffle($favs);
         for ($i = 0; $i < 3; $i++) : $fav = $favs[$i]; ?>
         <li><strong><a href="http://twitter.com/<?=$fav->user->screen_name?>/statuses/<?=$fav->id?>">@<?=$fav->user->screen_name?></a></strong><?=$fav->text?></li>
@@ -79,8 +121,16 @@ if (isset($_POST['email']) && $_POST['email'] && validEmail($_POST['email'])) {
     </section>
   </div>
   <footer>&copy; 2010 Left Logic Ltd &asymp; Previous years: <a href="http://2009.full-frontal.org">2009</a></footer>
-<script src="twitterlib.min.js"></script>
 <script src="html5.js"></script>
-<script src="holding2010.js"></script>
+<script>
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1656750-20']);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+})();
+</script>
 </body>
 </html>
