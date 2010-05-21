4a648f026e40172a8f09ff549de235380c6a6a92
remy <remy@remysharp.com>
2010-05-21 12:30:43 +0100

    Opening details by default

--- index.php
+++ index.php
@@ -89,14 +89,14 @@ Cheers,
     </section>
     <section class="subitems" id="wanttospeak">
       <h1>Want to speak?</h1>
-      <details>
+      <details open>
         <summary>Submit your abstract</summary>
         <p>If you&rsquo;re interested in speaking at our second event, <a href="mailto:events@leftlogic.com?subject=Speaker%20Proposal">drop us an email</a> with your talk title, description and a bit about yourself.</p>
       </details>
     </section>
     <section class="subitems" id="wanttosponsor">
       <h1>Want to sponsor?</h1>
-      <details>
+      <details open>
         <summary>Get in touch</summary>
         <p>Interested in sponsoring the Full Frontal JavaScript Conference? <a href="mailto:events@leftlogic.com">Get in touch</a> to discuss the opportunities available.</p>
       </details>
