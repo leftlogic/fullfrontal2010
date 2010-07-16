204728ffbbdd7c2e3bca51b6b099f0b9fe918262
remy <remy@remysharp.com>
2010-07-16 11:58:12 +0100

    analytics

--- includes/footer.php
+++ includes/footer.php
@@ -50,6 +50,17 @@
   <script src="/js/jquery.min.js"></script>
   <script src="/js/easing.js"></script>
   <script src="/js/base.js"></script>
+  <script>
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1656750-20']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+  })();
+  </script>
+  
 </body>
 </html>
 <?php
