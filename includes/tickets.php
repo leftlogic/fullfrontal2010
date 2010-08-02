f22cc8ebe80bf94d83d989d6b1929f9d9d239eaf
remy <remy@remysharp.com>
2010-08-02 18:07:26 +0100

    Tickets

---
+++ includes/tickets.php
@@ -0,0 +1,23 @@
+<article id="tickets" class="list">
+  <h2><span>Ticket types</span></h2>
+  <ol>
+    <li id="dayconf" class="box registration break link">
+      <a class="box" href="http://leftlogic.stagehq.com/events/167/booking/new">
+        <h3>Full Frontal day ticket (limited early bird price)</h3>
+        <h4>£100+VAT</h4>
+      </a>
+    </li>
+    <li id="html5" class="box registration break link">
+      <a class="box" href="http://leftlogic.stagehq.com/events/372/booking/new">
+        <h3>HTML5 JavaScript APIs full day workshop (incl. conference day ticket)</h3>
+        <h4>£350+VAT</h4>
+      </a>
+    </li>
+    <li id="phonegap" class="box registration break link">
+      <a class="box" href="http://leftlogic.stagehq.com/events/371/booking/new">
+        <h3>Mobile development with PhoneGap full day workshop (incl. conference day ticket)</h3>
+        <h4>£350+VAT</h4>
+      </a>
+    </li>
+  </ol>
+</article>
