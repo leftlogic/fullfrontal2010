ef81691696ef0a62d34b70470dcdbb6b3a8dd725
remy <remy@remysharp.com>
2010-08-08 11:24:18 +0100

    early bird expired

--- includes/tickets.php
+++ includes/tickets.php
@@ -3,8 +3,8 @@
   <ol>
     <li id="dayconf" class="box registration break link">
       <a class="box" href="http://leftlogic.stagehq.com/events/167/booking/new">
-        <h3>Full Frontal day ticket (limited early bird price)</h3>
-        <h4>£100+VAT</h4>
+        <h3>Full Frontal day ticket (limited early bird sold out)</h3>
+        <h4>£129+VAT</h4>
       </a>
     </li>
     <li id="html5" class="box registration break link">
