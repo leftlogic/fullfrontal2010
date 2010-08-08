9bca97cdd4e88898a4b4f067b568aaecc01a0dc5
remy <remy@remysharp.com>
2010-08-08 11:22:56 +0100

    350 -> 345 (typo)

--- includes/tickets.php
+++ includes/tickets.php
@@ -10,13 +10,13 @@
     <li id="html5" class="box registration break link">
       <a class="box" href="http://leftlogic.stagehq.com/events/372/booking/new">
         <h3>HTML5 JavaScript APIs full day workshop (incl. conference day ticket)</h3>
-        <h4>£350+VAT</h4>
+        <h4>£345+VAT</h4>
       </a>
     </li>
     <li id="phonegap" class="box registration break link">
       <a class="box" href="http://leftlogic.stagehq.com/events/371/booking/new">
         <h3>Mobile development with PhoneGap full day workshop (incl. conference day ticket)</h3>
-        <h4>£350+VAT</h4>
+        <h4>£345+VAT</h4>
       </a>
     </li>
   </ol>
