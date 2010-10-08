1ce0cbe4a79a0ff02bf8fccaffa9167386c5f833
remy <remy@remysharp.com>
2010-10-08 16:46:11 +0100

    Waiting list text

--- includes/tickets.php
+++ includes/tickets.php
@@ -20,6 +20,11 @@
       </a>
     </li>
   </ol>
-  
   <p>Please note that due to the way StageHQ (our payment system) works, the VAT won't show up separately when buying the tickets (nor in PayPal). VAT <em>has</em> been separately added, and our VAT registration is: 993 1266 95.</p>
+  
+  <div class="box">
+  <h3>Waiting list</h3>
+  <p>If you ticket type isn't available, <a href="mailto:events@leftlogic.com?subject=Waiting%20list">please email us</a> with your name and ticket type, and we'll add you to the waiting list and email you if the ticket becomes available.  This will be on a first come first serve basis.</p>
+  </div>
+  
 </article>
