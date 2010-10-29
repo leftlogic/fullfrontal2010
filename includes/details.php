8de18552bacb10e08a8f5b00d1ca0ce89cf4e810
remy <remy@remysharp.com>
2010-10-29 13:03:38 +0100

    Details page

---
+++ includes/details.php
@@ -0,0 +1,56 @@
+<div class="box">
+  <h1 style="font-size: 24px;">Conference Day Details</h1>
+<p>Please find below important and useful information to make sure your visit to the Full Frontal JavaScript Conference on Friday 12 November 2010 is a memorable experience.</p>
+
+<h2 id="directions">Directions</h2>
+
+<p>How to get there: <a href="http://goo.gl/VCo5">http://goo.gl/VCo5</a></p>
+
+<p>Full Frontal JavaScript Conference is being held at the historic Duke of York&#8217;s Picturehouse in Brighton.  It is centrally located on Preston Circus, less than 15 minutes walk from Brighton train station.  Trains run regularly from Gatwick Airport, London Bridge and London Victoria stations.  We recommend using http://www.traintimes.org.uk for up to date timetables.  Hotel recommendations can be found on the Full Frontal website: <a href="http://2010.full-frontal.org/venue/" title="Venue Details - Full Frontal - JavaScript Conference - 12th November 2010">http://2010.full-frontal.org/venue/</a> </p>
+
+<h2 id="registration">Registration</h2>
+
+<p>Registration opens at 9am.  You must bring a form of identification to pick up your delegate programme (bank card, driving licence etc).  Please keep your programme/pass with you at all times as you will need it to get back into the conference.</p>
+
+<p>Please let us know if you are unable to attend, we will accept replacements if we are informed prior to the day.</p>
+
+<h2 id="programme">Programme</h2>
+
+<p>The most up to date schedule and speaker abstracts can be found on the website, <a href="http://2010.full-frontal.org/">http://2010.full-frontal.org/</a></p>
+
+<p>Please feel free to photo or video any part of the conference.  Please use the hash tag #fullfrontalconf if you do upload or tweet so everyone else can see it too, and feel free to follow the Twitter account <a href="http://twitter.com/fullfrontalconf">@fullfrontalconf</a> for the latest info on the event.</p>
+
+<h2 id="technology">Technology</h2>
+
+<p>If you plan to bring a laptop, make sure it’s fully charged for the day, as we only have a few charging stations in the venue.</p>
+
+<p>There is wifi at the venue, but it’s not reliable. We would recommend either not relying on it all, tethering your mobile phone or bring your own mifi or dongle (the mobile reception is good).</p>
+
+<p>Also let us know if you want to be included in the <a href="http://twitter.com/fullfrontalconf/delegates10">@fullfrontalconf/delegates10</a> Twitter list (either drop us an email or send <a href="http://twitter.com/fullfrontalconf">@fullfrontalconf</a> a tweet).</p>
+
+<h2 id="refreshments">Refreshments</h2>
+
+<p>Tea, Coffee and Danishes will be available from 9am in the upstairs bar and the ground floor.  Further refreshments will be available from the same locations at the mid morning and mid afternoon breaks.  Please note lunch is not included but the programme includes local recommendations and maps, some of which can be found here: <a href="http://goo.gl/2gUy">http://goo.gl/2gUy</a></p>
+
+<h2 id="after_party">After Party</h2>
+
+<p>Please join us at the After Party at Oh So Social from 6.30pm-1am, directions will be included in the programme, also available here: <a href="http://goo.gl/reo1">http://goo.gl/reo1</a></p>
+
+<h2 id="pre_conference">Pre conference</h2>
+
+<p>If you&#8217;re coming down to Brighton on the Thursday, Async, the JavaScript Brighton meetup, are coming together for a special International Show and Tell evening. Available to all to join in where you can show off your latest and greatest JavaScript achievements (or you can just sit back and watch). Registration is free, but please let them know if you intend to go along (so they have enough chairs!). Details can be found here: <a href="http://asyncjs.com/showntell3/">http://asyncjs.com/showntell3/</a></p>
+
+<p>In the evening, around 9:30pm the Full Frontal organisers and speakers, along with the Async meetup, will be heading The Fountainhead pub until late: <a href="http://goo.gl/pm73">http://goo.gl/pm73</a></p>
+
+<h2 id="health_and_safety">Health and Safety</h2>
+
+<p>Please report any lost property to the registration desk.  Left Logic and Duke of York Picturehouse takes no responsibility for delegate&#8217;s lost property so we ask you to be vigilant at all times.</p>
+
+<p>The venue has at least one first-aider on duty at all times who can be contacted via the registration desk.</p>
+
+<p>The venue is fully accessible.  Left Logic would like to ensure that all delegates are able to have access therefore should you have any special requests please try to contact us in advance so we can accommodate your needs.</p>
+
+<p>If you have any questions or problems, feel free to email us at <a href="mailto:events@leftlogic.com">events@leftlogic.com</a></p>
+
+<p>We look forward to meeting you at the conference on Friday 12 November!</p>
+</div>
